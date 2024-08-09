c45af1902085bfa713e48607784cdce373ec4007
David Le Bansais <dlebansais@gmail.com>
2024-08-09 09:49:03 +0200

    Cleanup.

--- NUnit.ApplicationDomain/NUnit.ApplicationDomain/TaskWaitTestResultHandler.cs
+++ NUnit.ApplicationDomain/NUnit.ApplicationDomain/TaskWaitTestResultHandler.cs
@@ -1,18 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+namespace NUnit.ApplicationDomain;
 
-namespace NUnit.ApplicationDomain
+using global::System;
+using global::System.Threading.Tasks;
+
+/// <summary>
+///  A <see cref="IAsyncTestResultHandler"/> that simply invokes <see cref="Task.Wait()"/> on the
+///  result of a task-returning test.
+/// </summary>
+public class TaskWaitTestResultHandler : IAsyncTestResultHandler
 {
-  /// <summary>
-  ///  A <see cref="IAsyncTestResultHandler"/> that simply invokes <see cref="Task.Wait()"/> on the
-  ///  result of a task-returning test.
-  /// </summary>
-  public class TaskWaitTestResultHandler : IAsyncTestResultHandler
-  {
     /// <inheritdoc />
     public void Process(Task task)
-      => task.Wait();
-  }
+    {
+        if (task is null)
+            throw new ArgumentNullException(nameof(task));
+        task.Wait();
+    }
 }
